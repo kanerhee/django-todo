2826a6203567a9e91a02cb3733d38be03449527d
Phillip Kane Rhee <kanerhee@gmail.com>
2021-05-25 20:08:25 -0700

    removed keys

--- todowoo/settings.py
+++ todowoo/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=l0i5py_#2%tyfkr-$&$bxo)8)zx6--f6#sl=-o&23c#4$s$3&'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
